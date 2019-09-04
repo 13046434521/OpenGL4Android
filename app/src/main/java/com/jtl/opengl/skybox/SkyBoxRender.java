979e542964f734d68de4645283313a8517294f8f
JTL <604872657@qq.com>
2019-09-04 18:06:50 +0800

    天空盒  修改，不在  盒子中心

--- app/src/main/java/com/jtl/opengl/skybox/SkyBoxRender.java
+++ app/src/main/java/com/jtl/opengl/skybox/SkyBoxRender.java
@@ -9,11 +9,13 @@ import android.opengl.Matrix;
 
 import com.jtl.opengl.base.BaseRender;
 import com.jtl.opengl.helper.ShaderHelper;
+import com.socks.library.KLog;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
+import java.util.Arrays;
 
 /**
  * 作者:jtl
@@ -28,9 +30,12 @@ public class SkyBoxRender extends BaseRender {
     //    private static final String[] bitmapFilePath = new String[]{"model/sahara_lf.jpg", "model/sahara_rt.jpg",
 //            "model/sahara_dn.jpg", "model/sahara_up.jpg"
 //            , "model/sahara_ft.jpg", "model/sahara_bk.jpg"};
-    private static final String[] bitmapFilePath = new String[]{"model/left.jpg", "model/right.jpg",
-            "model/bottom.jpg", "model/top.jpg"
-            , "model/front.jpg", "model/back.jpg"};
+//    private static final String[] bitmapFilePath = new String[]{"model/left.jpg", "model/right.jpg",
+//            "model/bottom.jpg", "model/top.jpg"
+//            , "model/front.jpg", "model/back.jpg"};
+    private static final String[] bitmapFilePath = new String[]{"model/left.png", "model/right.png",
+            "model/bottom.png", "model/top.png"
+            , "model/front.png", "model/back.png"};
     private int mProgram;
     private int[] textureId = new int[1];
     private int a_Position;
@@ -49,7 +54,7 @@ public class SkyBoxRender extends BaseRender {
             1f, -1f, -1f,   //(7) Bottom-right far
     };
 
-    //    private float[] vertexCoord = new float[]{
+//        private float[] vertexCoord = new float[]{
 //            -0.5f, 0.5f, 0.5f,    //(0) Top-left near
 //            0.5f, 0.5f, 0.5f,     //(0.5) Top-right near
 //            -0.5f, -0.5f, 0.5f,   //(2) Bottom-left near
@@ -59,47 +64,47 @@ public class SkyBoxRender extends BaseRender {
 //            -0.5f, -0.5f, -0.5f,  //(6) Bottom-left far
 //            0.5f, -0.5f, -0.5f,   //(7) Bottom-right far
 //    };
-    private byte[] index = new byte[]{
-            1, 3, 0,
-            0, 3, 2,
-            1, 5, 3,
-            3, 5, 7,
-            7, 5, 6,
-            6, 5, 4,
-            4, 0, 6,
-            6, 0, 2,
-            1, 0, 4,
-            1, 4, 5,
-            7, 6, 3,
-            3, 6, 2,
-    };
-
-//    // 立方体索引
-//    private short[] index = new short[] {
-//            // Front
+//    private byte[] index = new byte[]{
 //            1, 3, 0,
 //            0, 3, 2,
-//
-//            // Back
-//            4, 6, 5,
-//            5, 6, 7,
-//
-//            // Left
-//            0, 2, 4,
-//            4, 2, 6,
-//
-//            // Right
-//            5, 7, 1,
-//            1, 7, 3,
-//
-//            // Top
-//            5, 1, 4,
-//            4, 1, 0,
-//
-//            // Bottom
-//            6, 2, 7,
-//            7, 2, 3
+//            1, 5, 3,
+//            3, 5, 7,
+//            7, 5, 6,
+//            6, 5, 4,
+//            4, 0, 6,
+//            6, 0, 2,
+//            1, 0, 4,
+//            1, 4, 5,
+//            7, 6, 3,
+//            3, 6, 2,
 //    };
+
+    // 立方体索引
+    private byte[] index = new byte[]{
+            // Front
+            1, 3, 0,
+            0, 3, 2,
+
+            // Back
+            4, 6, 5,
+            5, 6, 7,
+
+            // Left
+            0, 2, 4,
+            4, 2, 6,
+
+            // Right
+            5, 7, 1,
+            1, 7, 3,
+
+            // Top
+            5, 1, 4,
+            4, 1, 0,
+
+            // Bottom
+            6, 2, 7,
+            7, 2, 3
+    };
 
     private float[] viewMatrix = new float[16];
     private float[] projectionMatrix = new float[16];
@@ -192,14 +197,10 @@ public class SkyBoxRender extends BaseRender {
         GLES20.glTexParameteri(GLES20.GL_TEXTURE_CUBE_MAP, GLES20.GL_TEXTURE_WRAP_S, GLES20.GL_CLAMP_TO_EDGE);
         GLES20.glTexParameteri(GLES20.GL_TEXTURE_CUBE_MAP, GLES20.GL_TEXTURE_WRAP_T, GLES20.GL_CLAMP_TO_EDGE);
 
-        ShaderHelper.checkGLError(TAG, "initTexture");
         GLUtils.texImage2D(GLES20.GL_TEXTURE_CUBE_MAP_NEGATIVE_X, 0, mBitmaps[0], 0); // 左
-        ShaderHelper.checkGLError(TAG, "initTexture");
         GLUtils.texImage2D(GLES20.GL_TEXTURE_CUBE_MAP_POSITIVE_X, 0, mBitmaps[1], 0); // 右
-
         GLUtils.texImage2D(GLES20.GL_TEXTURE_CUBE_MAP_NEGATIVE_Y, 0, mBitmaps[2], 0); // 下
         GLUtils.texImage2D(GLES20.GL_TEXTURE_CUBE_MAP_POSITIVE_Y, 0, mBitmaps[3], 0); // 上
-
         GLUtils.texImage2D(GLES20.GL_TEXTURE_CUBE_MAP_NEGATIVE_Z, 0, mBitmaps[4], 0); // 前
         GLUtils.texImage2D(GLES20.GL_TEXTURE_CUBE_MAP_POSITIVE_Z, 0, mBitmaps[5], 0); // 后
 
@@ -208,7 +209,7 @@ public class SkyBoxRender extends BaseRender {
             bitmap.recycle();
         }
 
-        GLES20.glBindTexture(GLES20.GL_TEXTURE_CUBE_MAP, 0);
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, 0);
 
         ShaderHelper.checkGLError(TAG, "initTexture");
     }
@@ -221,27 +222,42 @@ public class SkyBoxRender extends BaseRender {
                 0.0f, 0.0f, 0.0f,
                 0.0f, 0.0f, -1.0f,
                 0f, 1f, 0f);
+        KLog.v(TAG, "rotateMatrix:" + Arrays.toString(rotateMatrix) + "\n"
+                + "viewMatrix:" + Arrays.toString(viewMatrix) + "\n");
+
         Matrix.multiplyMM(viewMatrix, 0, viewMatrix, 0, rotateMatrix, 0);
-//        Matrix.rotateM(viewMatrix, 0, 90, 1f, 0f, 0f);
+        KLog.d(TAG, "rotateMatrix:" + Arrays.toString(rotateMatrix) + "\n"
+                + "viewMatrix:" + Arrays.toString(viewMatrix) + "\n");
+
+        Matrix.rotateM(viewMatrix, 0, 90, 1f, 0f, 0f);
         Matrix.multiplyMM(mvpMatrix, 0, projectionMatrix, 0, viewMatrix, 0);
+
+        KLog.w(TAG, "rotateMatrix:" + Arrays.toString(rotateMatrix) + "\n"
+                + "viewMatrix:" + Arrays.toString(viewMatrix) + "\n"
+                + "projectionMatrix:" + Arrays.toString(projectionMatrix) + "\n"
+                + "mvpMatrix:" + Arrays.toString(mvpMatrix) + "\n");
+
     }
 
     @Override
     protected void onDraw() {
         GLES20.glUseProgram(mProgram);
-        GLES20.glEnable(GLES20.GL_DEPTH_TEST);
+//        GLES20.glEnable(GLES20.GL_DEPTH_TEST);
+//        GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
         GLES20.glBindTexture(GLES20.GL_TEXTURE_CUBE_MAP, textureId[0]);
-
+        GLES20.glUniform1i(u_TextureUnit, 0);
         GLES20.glUniformMatrix4fv(u_MvpMatrix, 1, false, mvpMatrix, 0);
 
         GLES20.glEnableVertexAttribArray(a_Position);
-
         GLES20.glVertexAttribPointer(a_Position, 3, GLES20.GL_FLOAT, false, 0, mVertexCoord);
 
         GLES20.glDrawElements(GLES20.GL_TRIANGLES, index.length, GLES20.GL_UNSIGNED_BYTE, mIndexBuffer);
+
         GLES20.glEnableVertexAttribArray(a_Position);
+
+
         GLES20.glBindTexture(GLES20.GL_TEXTURE_CUBE_MAP, 0);
-        GLES20.glUseProgram(mProgram);
+        GLES20.glUseProgram(0);
 
         ShaderHelper.checkGLError(TAG, "onDraw");
     }
